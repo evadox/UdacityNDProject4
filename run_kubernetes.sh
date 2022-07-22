b07981bbc95a80d25a9466453ce311a336774188
Mr Taiwo <taiwo@DESKTOP-GBN40KV.localdomain>
2022-07-22 20:57:45 +0100

    add files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="todak2000/cloudprojectfour:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run cloudprojectfour --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward cloudprojectfour 8000:80
+
+
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=todak2000/cloudprojectfour
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u todak2000
+docker tag cloudprojectfour $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
